70f93a68f8513a1c9ba4ff80f7b4216723554115
tcaironglnan <995956062@qq.com>
2019-02-14 12:12:09 +0800

    123

--- server/src/main/java/com/server/ServerApplication.java
+++ server/src/main/java/com/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class ServerApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(ServerApplication.class, args);
-        new SpringApplicationBuilder(ServerApplication.class).web(true).run(args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 }
